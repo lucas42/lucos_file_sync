7cf04bfc854467f260a52333147504d92b184736
Luke Blaney <git@lukeblaney.co.uk>
2020-12-05 14:38:41 +0000

    Pass in tcp port number as environment variable

--- agent.rs
+++ agent.rs
@@ -1,3 +1,8 @@
+use std::env;
+
 fn main() {
-	println!("Running Agent");
+	let port = env::var("PORT").expect("Environment Varible PORT must be set")
+		.parse::<u16>().expect("Environment Varible PORT must be an integer");
+
+	println!("Running Agent {}", port);
 }
